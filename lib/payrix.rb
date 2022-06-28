54a0b5ad455a1cbf3f666aca47d573b7b445470e
Raj Kumar Shrestha <raj@lookedafter.com>
2022-06-28 15:27:07 +1000

    require all the files into main class

--- lib/payrix.rb
+++ lib/payrix.rb
@@ -1,19 +1,41 @@
+# frozen_string_literal: true
+
 require "payrix/version"
 require "payrix/configuration"
-require "payrix/exceptions"
-require "payrix/http"
-require "payrix/resource"
+require "payrix/errors"
+require "payrix/code_description"
+
+# hhtp_operation
+require "payrix/http_operation/get"
+require "payrix/http_operation/post"
+require "payrix/http_operation/put"
+require "payrix/http_operation/delete"
+
+# http
+require "payrix/http/request_params"
+require "payrix/http/request"
+require "payrix/http/response"
+require "payrix/http/error_response"
+
+# Resources
+require "payrix/resource/base"
+require "payrix/resource/auth"
+require 'payrix/api_auth'
+require "payrix/resource/businesses"
+require "payrix/resource/payers"
+require "payrix/resource/transactions"
+require "payrix/resource/bpay"
 
 module Payrix
   class << self
-    attr_writer :configuration
-  end
+    # attr_writer :configuration
 
-  def self.configuration(auth: false, sandbox: false)
-    @configuration ||= Configuration.new(auth: auth, sandbox: sandbox)
-  end
+    def configuration(sandbox: true)
+      @configuration ||= Configuration.new(sandbox)
+    end
 
-  def self.configure
-    yield(configuration)
+    def configure(sandbox: false)
+      yield(configuration(sandbox: sandbox))
+    end
   end
 end
